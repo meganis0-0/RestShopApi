f70fb6f99deec82fba9e024ffd931e40e71d0cd9
MEGANIS <->
2024-11-21 16:03:35 +0300

    FloatField for price

--- onlinestore/shop/models.py
+++ onlinestore/shop/models.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 class Good(models.Model):
     name = models.CharField(max_length=100)
     amount = models.IntegerField()
-    price = models.IntegerField()
+    price = models.FloatField()
 
 
 #модель для токена
